d104f8fea64d6f3bbb512c32685692aa359cbf41
Ludovic <74627383+LudovicLelong@users.noreply.github.com>
2021-02-25 21:37:21 +0100

    Add files via upload

---
+++ cours API/application.js
@@ -0,0 +1,82 @@
+
+
+// movie-App
+
+// (a supprimùer si pas utiliser ) const APIKEY = "04c3573&a5ee918f014970082a0088b1"
+const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=';
+
+
+
+async function getMovies(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+    const main = document.getElementById('main');
+    const form = document.getElementById('form');
+    const search = document.getElementById('search');
+
+    console.log(respData);
+    
+    showMovies(respData.results);
+    
+}
+
+//intinialisation des films les plus populaires 
+getMovies(APIURL);
+
+function showMovies(movies) {
+    //clear main
+    main.innerHTML = "";
+    movies.forEach(movie => {
+        const { poster_path, title, vote_average, overview} = movie;
+
+        const movieE1 = document.createElement('div');
+        movieE1.classList.add('movie');
+
+        movieE1.innerHTML = ` 
+            <img src="${IMGPATH + poster_path}" 
+             alt="${title}" >
+            <div class="movieInfo">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h4>Synopsis</h4>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieE1);
+
+    });
+} 
+
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
+
+// Faire imput envoie cherche film - série
+form = addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
+
+
+
+
+
